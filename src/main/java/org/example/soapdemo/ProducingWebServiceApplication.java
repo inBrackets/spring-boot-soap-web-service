32bd89c909191fc2225a531866d8e6206493317f
Jason <extremewinds@gmail.com>
2025-01-19 16:28:04 +0100

    add other files

--- src/main/java/org/example/soapdemo/ProducingWebServiceApplication.java
+++ src/main/java/org/example/soapdemo/ProducingWebServiceApplication.java
@@ -1,2 +1,12 @@
-package org.example.soapdemo;public class Application {
+package org.example.soapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducingWebServiceApplication.class, args);
+    }
 }
--- src/main/java/org/example/soapdemo/producingwebservice/CountryRepository.java
+++
@@ -1,2 +0,0 @@
-package org.example.soapdemo.producingwebservice;public class CountryRepository {
-}
